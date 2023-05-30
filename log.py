781223d85a7e1fb79d8ebd7dc2c619143b45ce71
Nojus <me@xe1h.xyz>
2023-05-30 23:34:17 +0300

    log file

--- log.py
+++ log.py
@@ -1,4 +1,10 @@
 from flask import request
+from datetime import datetime
+
+# Create log file if it doesn't exist
+f = open("log.txt", "a")
+
+
 def log(message, route=None):
     if route is None:
         try:
@@ -6,9 +12,13 @@ def log(message, route=None):
         except:
             route = 'APP'
 
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
     BLUE = '\033[94m'
     GREEN = '\033[92m'
+    CYAN = '\033[96m'
     BOLD = '\033[1m'
     ENDC = '\033[0m'
 
-    print(BLUE + BOLD + " * " + route + ENDC + GREEN + ' | ' + ENDC + message)
+    print(BLUE + BOLD + " * " + route + ENDC + GREEN + ' | ' + CYAN + str(timestamp) + GREEN + " | " + ENDC + message)
+    f.write(f"{timestamp} | {route} | {message}\n")
